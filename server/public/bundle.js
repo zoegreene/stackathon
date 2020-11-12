38bb54869b347d4d118a4479767d68cec8ad6c35
Zoe Greene <zoe.greene12@gmail.com>
2020-11-11 23:47:07 -0500

    improve run time

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -2406,7 +2406,7 @@ const createPlaylist = (user, hashParam, name, duration, savedSongs) => {
         };
       });
       console.log(simpleSavedSongs);
-      const songsToAdd = knapsack(duration * 60, simpleSavedSongs, simpleSavedSongs.length);
+      const songsToAdd = knapsack(simpleSavedSongs, duration * 60);
       console.log('songs to add: ', songsToAdd); // STORE THOSE SONGS INTO THE NEWLY CREATED PLAYLIST
       // const songLengths = savedSongs.map(song => getSongLength(song.track.id, hashParam));
       // console.log(getPlaylistDuration(savedSongs, hashParam));
@@ -2425,34 +2425,73 @@ const getSongLength = async (songId, hashParam) => {
     }
   });
   return songLength.data.track.duration;
-};
+}; // const getPlaylistDurationv1 = (playlist) => {
+//     const duration = playlist.length > 0 ? 
+//         playlist.reduce((sum, song) => sum + song.songLength, 0) : 0;
+//     return duration;
+// }
+// const knapsackv1 = (runningDuration, savedSongs, n, playlist = []) => {
+//     console.log('playlist', playlist);
+//     // Base case
+//     if (runningDuration === 0 || n === 0) {
+//         return playlist;
+//     }
+//     if (savedSongs[n - 1].songLength > runningDuration) {
+//         return knapsack(runningDuration, savedSongs, n - 1, playlist);
+//     }
+//     else {
+//         const newPlaylist = [...playlist];
+//         newPlaylist.push(savedSongs[n - 1]);
+//         let included = knapsack(runningDuration - savedSongs[n - 1].songLength, savedSongs, n - 1, newPlaylist);
+//         let excluded = knapsack(runningDuration, savedSongs, n - 1, playlist);
+//         if (getPlaylistDuration(included) > getPlaylistDuration(excluded)) {
+//             return included;
+//         } else {
+//             return excluded;
+//         }
+//     }
+// }
 
-const getPlaylistDuration = playlist => {
-  const duration = playlist.length > 0 ? playlist.reduce((sum, song) => sum + song.songLength, 0) : 0;
-  return duration;
-};
 
-const knapsack = (runningDuration, savedSongs, n, playlist = []) => {
-  console.log('playlist', playlist); // Base case
+const knapsack = (savedSongs, duration) => {
+  let cache = []; // set up empty cache matrix
 
-  if (runningDuration === 0 || n === 0) {
-    return playlist;
-  }
+  for (let x = 0; x < savedSongs.length + 1; x++) {
+    cache[x] = [];
 
-  if (savedSongs[n - 1].songLength > runningDuration) {
-    return knapsack(runningDuration, savedSongs, n - 1, playlist);
-  } else {
-    const newPlaylist = [...playlist];
-    newPlaylist.push(savedSongs[n - 1]);
-    let included = knapsack(runningDuration - savedSongs[n - 1].songLength, savedSongs, n - 1, newPlaylist);
-    let excluded = knapsack(runningDuration, savedSongs, n - 1, playlist);
-
-    if (getPlaylistDuration(included) > getPlaylistDuration(excluded)) {
-      return included;
-    } else {
-      return excluded;
+    for (let y = 0; y < duration + 1; y++) {
+      cache[x][y] = [];
     }
   }
+
+  const weights = savedSongs.map(song => song.songLength);
+  console.log('weights', weights);
+  const values = savedSongs.map(song => song.songId);
+  console.log('values', values);
+
+  for (let i = 0; i < savedSongs.length + 1; i++) {
+    for (let j = 0; j < duration + 1; j++) {
+      if (i === 0 || j === 0) {
+        cache[i][j] = [];
+      } else if (weights[i - 1] <= j) {
+        const copy = [...cache[i - 1][j - weights[i - 1]]];
+        copy.push(values[i - 1]);
+        const included = copy;
+        const excluded = cache[i - 1][j];
+        cache[i][j] = getPlaylistDuration(included, savedSongs) > getPlaylistDuration(excluded, savedSongs) ? included : excluded;
+      } else {
+        cache[i][j] = cache[i - 1][j];
+      }
+    }
+  }
+
+  return cache[savedSongs.length][duration];
+};
+
+const getPlaylistDuration = (playlist, savedSongs) => {
+  return playlist.length > 0 ? playlist.reduce((sum, elem) => {
+    return savedSongs.find(song => song.id === elem).songLength + sum;
+  }, 0) : 0;
 };
 
 function userReducer(state = {}, action) {
