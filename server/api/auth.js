06d46a2f64b5dfcca921552cf36591d4c09c1721
Zoe Greene <zoe.greene12@gmail.com>
2020-11-13 22:32:11 -0500

    new algo for knapsack

--- server/api/auth.js
+++ server/api/auth.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 const client_id = process.env.client_id;
 const client_secret = process.env.client_secret;
 
-const redirect_uri = 'http://localhost:8888/api/auth/callback'; // Your redirect uri
+// const redirect_uri = 'http://localhost:8888/api/auth/callback'; // Your redirect uri
+const redirect_uri = 'https://floating-woodland-30370.herokuapp.com/api/auth/callback'
 const stateKey = 'spotify_auth_state';
 
 
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -2466,7 +2466,7 @@ const createPlaylist = (user, hashParam, name, duration, savedSongs) => {
       let songsToAdd = knapsack(simpleSavedSongs, duration * 60); // STORE THOSE SONGS INTO THE NEWLY CREATED PLAYLIST
       // format JSON object for request body
 
-      songsToAdd = songsToAdd.map(song => `spotify:track:${song}`); // Don't save the result, you want the playlist ID to be stored in the store
+      songsToAdd = [...songsToAdd].map(song => `spotify:track:${song.songId}`); // Don't save the result, you want the playlist ID to be stored in the store
 
       await axios__WEBPACK_IMPORTED_MODULE_0___default().post(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
         "uris": songsToAdd
@@ -2491,12 +2491,6 @@ const getSongLength = async (songId, hashParam) => {
   return songLength.data.track.duration;
 };
 
-const getPlaylistDuration = (playlist, savedSongs) => {
-  return playlist.length > 0 ? playlist.reduce((sum, elem) => {
-    return savedSongs.find(song => song.songId === elem).songLength + sum;
-  }, 0) : 0;
-};
-
 const getSavedSongsDuration = async (savedSongs, hashParam) => {
   let duration = await Promise.all(savedSongs.map(async song => {
     const songLength = await getSongLength(song.track.id, hashParam);
@@ -2505,6 +2499,19 @@ const getSavedSongsDuration = async (savedSongs, hashParam) => {
   return duration.reduce((sum, elem) => sum + elem);
 };
 
+class Playlist extends Set {
+  constructor() {
+    super();
+    this.duration = 0;
+  }
+
+  add(song) {
+    super.add(song);
+    this.duration += song.songLength;
+  }
+
+}
+
 const knapsack = (savedSongs, duration) => {
   let cache = []; // set up empty cache matrix
 
@@ -2512,23 +2519,24 @@ const knapsack = (savedSongs, duration) => {
     cache[x] = [];
 
     for (let y = 0; y < duration + 1; y++) {
-      cache[x][y] = [];
+      cache[x][y] = new Playlist();
     }
   }
 
   const weights = savedSongs.map(song => song.songLength);
-  const values = savedSongs.map(song => song.songId);
+  const values = savedSongs;
 
   for (let i = 0; i < savedSongs.length + 1; i++) {
     for (let j = 0; j < duration + 1; j++) {
       if (i === 0 || j === 0) {
-        cache[i][j] = [];
+        cache[i][j] = new Playlist();
       } else if (weights[i - 1] <= j) {
-        const copy = [...cache[i - 1][j - weights[i - 1]]];
-        copy.push(values[i - 1]);
-        const included = copy;
-        const excluded = cache[i - 1][j];
-        cache[i][j] = getPlaylistDuration(included, savedSongs) > getPlaylistDuration(excluded, savedSongs) ? included : excluded;
+        const excluded = new Playlist();
+        cache[i - 1][j].forEach(song => excluded.add(song));
+        const included = new Playlist();
+        cache[i - 1][j - weights[i - 1]].forEach(song => included.add(song));
+        included.add(values[i - 1]);
+        cache[i][j] = included.duration > excluded.duration ? included : excluded;
       } else {
         cache[i][j] = cache[i - 1][j];
       }
@@ -2536,7 +2544,42 @@ const knapsack = (savedSongs, duration) => {
   }
 
   return cache[savedSongs.length][duration];
-};
+}; // ALTERNATE ALGORITHM FOR KNAPSACK:
+// const knapsack = (savedSongs, duration) => {
+//     let cache = [];
+//     // set up empty cache matrix
+//     for (let x = 0; x < savedSongs.length + 1; x++) {
+//         cache[x] = [];
+//         for (let y = 0; y < duration + 1; y++) {
+//             cache[x][y] = [];
+//         }
+//     }
+//     const weights = savedSongs.map(song => song.songLength);
+//     const values = savedSongs.map(song => song.songId);
+//     for (let i = 0; i < savedSongs.length + 1; i++) {
+//         for (let j = 0; j < duration + 1; j++) {
+//             if (i === 0 || j === 0 ) {
+//                 cache[i][j] = [];
+//             } else if (weights[i - 1] <= j) {
+//                 const copy = [...cache[i - 1][j - weights[i - 1]]];
+//                 copy.push(values[i - 1]);
+//                 const included = copy;
+//                 const excluded = cache[i - 1][j];
+//                 cache[i][j] = getPlaylistDuration(included, savedSongs) > getPlaylistDuration(excluded, savedSongs) ? included : excluded;
+//             } else {
+//                 cache[i][j] = cache[i - 1][j];
+//             }
+//         }
+//     }
+//     return cache[savedSongs.length][duration];
+// }
+// const getPlaylistDuration = (playlist, savedSongs) => {
+//     return playlist.length > 0 ? 
+//         playlist.reduce((sum, elem) => {
+//             return savedSongs.find(song => song.songId === elem).songLength + sum;
+//         }, 0) : 0;
+// }
+
 
 function userReducer(state = {}, action) {
   switch (action.type) {
